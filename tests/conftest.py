c45efc638a2215f6e0507132bc0b4da2b9c47ab1
andrepsousa <you@example.com>
2025-08-13 09:49:41 -0300

    test: add conftest to ensure src is importable in CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,4 +3,4 @@ import sys
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(_file_), ".."))
 if PROJECT_ROOT not in sys.path:
-    sys.path.insert(0, PROJECT_ROOT)
+    sys.path.insert(0, PROJECT_ROOT)
